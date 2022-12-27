7700d2ad18cc668095ae6c42874e710386d94723
unknown <Dmitiy_yar@mail.ru>
2022-12-27 10:59:14 +0300

    add fixed task2

--- task1.py
+++ task1.py
@@ -1,42 +1,40 @@
 import requests
 import telebot
+from telebot import types
 
 
-def handle_game_proc(message):
+def handle_game_proc():
     res = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
     lst = list(res['Valute'])
-    try:
-        if lst.count(message.text) > 0:
-            return True
-        else:
-            return False
-    except KeyError:
-        if lst.count(message.text) > 0:
-            return True
-        else:
-            return False
+    return lst
 
 
-bot = telebot.TeleBot("####################################")
+bot = telebot.TeleBot("##############################")
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=handle_game_proc())
+def game_process(message):
+    res = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
+    res = res['Valute'][message.text[1:]
+                        ]['Name'], res['Valute'][message.text[1:]]['Value']
+    res = ' '.join(map(str, res))
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn = types.KeyboardButton('Коды валют')
+    markup.add(btn)
+    bot.send_message(message.chat.id, res, reply_markup=markup)
+
+
+@bot.message_handler(content_types=["text"])
 def send_welcome(message):
     res = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
     lst = list(res['Valute'])
     lst1 = []
     for i in lst:
-        i += f" - {res['Valute'][i]['Name']}\n"
+        i = f"/{i} - {res['Valute'][i]['Name']}\n"
         lst1.append(i)
-    x = ''.join(lst1)
-    bot.reply_to(message, f"Выберите код валюты\n{x}")
+    str = ''.join(lst1)
+    bot.send_message(
+        message.chat.id, f"Выберите код валюты\n\n\n{str}")
 
-
-@bot.message_handler(func=handle_game_proc)
-def game_process(message):
-    res = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
-    res = res['Valute'][message.text]['Name'], res['Valute'][message.text]['Value']
-    res = ' '.join(map(str, res))
-    bot.reply_to(message, res)
 
 bot.infinity_polling()
